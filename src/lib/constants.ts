f50df349b1713e7ec013f8039fee1366a0c6a7f2
erquette <80589692+erquette@users.noreply.github.com>
2026-03-21 12:35:45 -0600

    feat: add dropzone to upload files

---
+++ src/lib/constants.ts
@@ -0,0 +1,8 @@
+export const ACCEPTED_IMAGE_TYPES = {
+  "image/jpeg": [".jpg", ".jpeg"],
+  "image/png": [".png"],
+  "image/webp": [".webp"],
+}
+
+export const MAX_FILE_SIZE_MB = 20
+export const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024
